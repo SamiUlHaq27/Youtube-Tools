f27b6d5e8ffdd7876940dd664735d75ccf080ec4
SamiUlHaq27 <134762645+SamiUlHaq27@users.noreply.github.com>
2024-05-22 21:31:58 +0500

    Functionality Complete
    Complete website is functional but designing is required.

--- Youtube/dwn/views.py
+++ Youtube/dwn/views.py
@@ -5,15 +5,15 @@ from django.views import View
 import requests
 import json
 
-"""https://youtu.be/aM9-zFvH6kI?si=JQ5uOV27NeM5JwZW"""
+# """https://youtu.be/aM9-zFvH6kI?si=JQ5uOV27NeM5JwZW"""
 
-def download(link, file_name):
-    r = requests.get(link, stream = True)
-    with open(BASE_DIR/'media'/'output'/file_name, 'wb') as f: 
-        for chunk in r.iter_content(chunk_size = 1024*1024): 
-            if chunk: 
-                f.write(chunk) 
-    return "/media/output/"+file_name
+# def download(link, file_name):
+#     r = requests.get(link, stream = True)
+#     with open(BASE_DIR/'media'/'output'/file_name, 'wb') as f: 
+#         for chunk in r.iter_content(chunk_size = 1024*1024): 
+#             if chunk: 
+#                 f.write(chunk) 
+#     return "/media/output/"+file_name
 
 
 class Video(View):
@@ -22,23 +22,29 @@ class Video(View):
     display_streams = False
     
     def get(self, request):
-        url = request.GET.get("url","")
+        url :str = request.GET.get("url","")
+        if url.find("list=") > -1:
+            return redirect("playlist/?purl="+url)
         if url != "":
             try:
                 youtube_object = pytube_mng.get_object(url)
-                self.display_streams = True
+                self.display_streams = True 
+                description = youtube_object.description
+                if description == None:
+                    description = ""        
                 return render(request, 'dwn/index.html', {
                     "title":youtube_object.title,
                     "duration":pytube_mng.get_length(youtube_object),
                     "thumbnail":youtube_object.thumbnail_url,
                     "streams":youtube_object.streams,
-                    "description":youtube_object.description,
+                    "description":description,
                     "link":"value="+url,
                     "error":self.display_error,
                     "display_streams":self.display_streams,
                     })
             except Exception as e:
                 print(e)
+                self.display_streams = False
                 self.display_error = True
         return render(request, "dwn/index.html", {
             "error":self.display_error,
@@ -58,6 +64,8 @@ class Playlist(View):
     
     def get(self, request):
         url = request.GET.get("purl", "")
+        if url.find("watch?v=") > -1:
+            return redirect("/?url="+url)
         if url != "":
             try:
                 playlist_object = pytube_mng.get_playlist(url)
@@ -66,7 +74,7 @@ class Playlist(View):
                 
                 title = playlist_object.title
                 length = playlist_object.length
-                thumbnail = videos[0].thumbnail_url,
+                thumbnail = videos[0].thumbnail_url
                 
                 videos = pytube_mng.get_videos_data(videos)
                 self.display_videos = True
@@ -102,10 +110,14 @@ class PlaylistDownload(View):
     def get(self, request):
         videos = request.session.get("videos", False)
         if videos:
-            # for i in range(len(videos)):
-            #     videos[i]["streams"] = pytube_mng.get_streams_data(pytube_mng.get_object(videos[i]["url"]))
-            with open("out.json","r") as f:
-                videos = json.load(f)
+            for i in range(len(videos)):
+                try: 
+                    videos[i]["streams"] = pytube_mng.get_streams_data(pytube_mng.get_object(videos[i]["url"]))
+                    print(f"Video {i:>3}: fetched")
+                except Exception as e:
+                    print(f"Video {i:>3}: not fetched due to "+e.__str__)
+            # with open("playlist.json","r") as f:
+            #     videos = json.load(f)
             request.session["videos"] = videos
             return render(request, "dwn/download_playlist.html", {"videos":videos})
         else:
@@ -123,12 +135,12 @@ class PlaylistDownload(View):
                     download.append({
                         "video_no":video_no,
                         "stream_no":stream_no,
-                        "filename":videos[video_no]["streams"][stream_no]["default_filename"],
-                        "url":videos[video_no]["streams"][stream_no]["url"],
+                        "filename":videos[video_no-1]["streams"][stream_no-1]["default_filename"],
+                        "url":videos[video_no-1]["streams"][stream_no-1]["url"],
+                        "resolution":videos[video_no-1]["streams"][stream_no-1]["resolution"],
+                        "format":videos[video_no-1]["streams"][stream_no-1]["mime_type"]
                     })
-                    
-            print(download)
             
-            return render(request, "dwn/download_playlist.html", {"videos":videos})
+            return render(request, "dwn/download_list.html", {"downloads":download})
         else:
             return redirect("playlist")
